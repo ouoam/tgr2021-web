c9b013f67d06d896744fb3cfd28aa0a3647e39fe
Phumphathai Cha <ouoam2555@gmail.com>
2021-02-24 00:05:35 +0700

    Move find form to component

---
+++ resources/js/components/FindForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <form class="form-inline" method="get" id="myForm">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <select class="form-control" id="name" name="name">
+                <option value=""></option>
+                <option v-for="kind in kinds" :key="kind" :value="kind" :selected="name==kind">
+                    {{ kind }}
+                </option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="qty">Qty</label>
+            <input type="number" class="form-control" id="qty" name="qty" placeholder="qty" v-model="qty">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</template>
+<script>
+    export default {
+        props: ['init_qty', 'init_name', 'kinds'],
+        data: function () {
+            return {
+                qty: this.init_qty,
+                name: this.init_name
+            }
+        }
+    }
+</script>
--- resources/views/find.blade.php
+++ resources/views/find.blade.php
@@ -12,27 +12,9 @@
         <div class="container">
             <div id="app">
                 <div class="row">
-                    <form class="form-inline" method="get" id="myForm">
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <select class="form-control" id="name" name="name">
-                                <option value=""></option>
-                                @foreach ($kinds as $kind)
-                                @if (Request::input('name') == $kind)
-                                <option value="{{$kind}}" selected>{{$kind}}</option>
-                                @else
-                                <option value="{{$kind}}">{{$kind}}</option>
-                                @endif
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="qty">Qty</label>
-                            <input type="number" class="form-control" id="qty" name="qty" placeholder="qty" value="{{Request::input('qty')}}">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                    <find-form init_qty='{{Request::input("qty")}}' init_name='{{Request::input("name")}}' :kinds='@json($kinds, JSON_HEX_QUOT)'/>
                 </div>
+
                 <div class="row">
                     @include('table', ['items' => $items])
                 </div>
